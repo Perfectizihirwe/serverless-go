b37167960e1e125b892f2c34e312ce95454e5ea7
Perfectizihirwe <perfectizihirwe@gmail.com>
2023-09-18 00:05:11 +0200

    Add deployment config

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/Perfectizihirwe/serverless-go-api
 go 1.21.1
 
 require (
+	github.com/aws/aws-lambda-go v1.41.0 // indirect
+	github.com/awslabs/aws-lambda-go-api-proxy v0.15.0 // indirect
 	github.com/bytedance/sonic v1.10.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
--- main.go
+++ main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"github.com/Perfectizihirwe/serverless-go-api/initializers"
 	"github.com/Perfectizihirwe/serverless-go-api/models"
 	"github.com/Perfectizihirwe/serverless-go-api/routes"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/gin-gonic/gin"
 )
 
+var ginLambda *ginadapter.GinLambda
+
 func init() {
+	log.Printf("Gin cold start")
 	initializers.LoadEnvVariables()
 	initializers.LoadDatabase()
 	initializers.DB.AutoMigrate(&models.Post{})
+	app := gin.Default()
+	router := app.Group("/api/v1")
+	app.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	routes.AddRoutes(router)
+
+	ginLambda = ginadapter.New(app)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return ginLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
-	app := gin.New()
-	router := app.Group("/api/v1")
-	routes.AddRoutes(router)
-
-	app.Run()
+	lambda.Start(Handler)
 }
